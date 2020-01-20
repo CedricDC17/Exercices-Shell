70005f48381b9d7c233d4be1d7a35e3a48211ce6
CedricDC17 <cedric.dc91@gmail.com>
2020-01-20 15:07:17 +0100

    slave real

--- shell_exercices/exo1.sh
+++ shell_exercices/exo1.sh
@@ -6,4 +6,4 @@ Presents=10
 BirthDay=`date -d "$BirthDate" +%A`
 echo "I was born the $BirthDate or $BirthDateLetters today, i got $Presents presents and the day is $BirthDay so i don't have school :) "
 
-
+echo "but slave is real"
