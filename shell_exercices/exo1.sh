63735b50c303035d25b05db7488e5633061e6e62
CedricDC17 <cedric.dc91@gmail.com>
2020-01-20 14:59:54 +0100

    added the date in letters

--- shell_exercices/exo1.sh
+++ shell_exercices/exo1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "exo 1 tmtc"
-BirthDate=01/01/2000
+BirthDate="Oct 17, 2000"
 Presents=10
 BirthDay=`date -d "$BirthDate" +%A`
-echo "I was born the $BirthDate today, i got $Presents presents and the day is $BirthDay so i don't have school :) "
+echo "I was born the $BirthDate or $BirthDateLetters today, i got $Presents presents and the day is $BirthDay so i don't have school :) "
 
 
